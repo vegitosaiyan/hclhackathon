503618ae689e2d568d9e2e4181bafcb20f3d639c
Prabhakaran G <prabhakaransaai@gmail.com>
2025-11-08 15:26:33 +0530

    model commit
    hc model commit

---
+++ src/main/java/com/hclhackathon/model/Merchant.java
@@ -0,0 +1,86 @@
+package com.hclhackathon.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "merchant")
+public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long merchantId;
+
+    @Column(nullable = false, length = 100)
+    private String merchantName;
+
+    @Column(length = 100)
+    private String merchantEmail;
+
+    @Column(length = 30)
+    private String bankAccountNo;
+
+    @Column(length = 20)
+    private String status = "ACTIVE";
+
+    @OneToMany(mappedBy = "merchant")
+    private List<TransactionLedger> transactions;
+
+	public Long getMerchantId() {
+		return merchantId;
+	}
+
+	public void setMerchantId(Long merchantId) {
+		this.merchantId = merchantId;
+	}
+
+	public String getMerchantName() {
+		return merchantName;
+	}
+
+	public void setMerchantName(String merchantName) {
+		this.merchantName = merchantName;
+	}
+
+	public String getMerchantEmail() {
+		return merchantEmail;
+	}
+
+	public void setMerchantEmail(String merchantEmail) {
+		this.merchantEmail = merchantEmail;
+	}
+
+	public String getBankAccountNo() {
+		return bankAccountNo;
+	}
+
+	public void setBankAccountNo(String bankAccountNo) {
+		this.bankAccountNo = bankAccountNo;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<TransactionLedger> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<TransactionLedger> transactions) {
+		this.transactions = transactions;
+	}
+
+
+}
--- src/main/java/com/hclhackathon/model/TransactionLedger.java
+++ src/main/java/com/hclhackathon/model/TransactionLedger.java
@@ -1,4 +1,5 @@
 package com.hclhackathon.model;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
